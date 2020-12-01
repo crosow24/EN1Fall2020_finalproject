aab0d454d09f4bff06afbdcf4a272d096b96cbc3
crosow24 <connor.rosow@gmail.com>
2020-12-01 12:54:39 -0500

    keys

--- src/secret.js
+++ src/secret.js
@@ -16,8 +16,8 @@ var getAPIusername = ""; // fill in with your name (if left blank, assuming usin
 // (2) Hardcode your API key information here
 
 // AIRTABLE SUPPORT
-var airtable_api = "key" + "XXXXXXXXXXX";     // fill in your info
-var airtable_baseid = "app" + "YYYYYYYYYYYY"; // fill in your info
+var airtable_api = "key" + "e2rALYh9PFqsdD";     // fill in your info
+var airtable_baseid = "app" + "CM3ghi1YM7X0yU"; // fill in your info
 
 // SYSTEMLINK SUPPORT
 var systemlink_api = "XXXXXXXXXXXXXXXXXX"; // fill in your info
